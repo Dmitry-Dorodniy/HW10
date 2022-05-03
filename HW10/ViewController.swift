ee9cb80f5272b4f29749194625c26047f13a634a
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-05-03 19:34:26 +0300

    Initial Commit

---
+++ HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10
+//
+//  Created by Dmitry Dorodniy on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
